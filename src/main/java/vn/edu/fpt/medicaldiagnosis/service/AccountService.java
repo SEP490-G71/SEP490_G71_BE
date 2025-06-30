aa047082e52ba0fd19e34c7104ca7e9588a05562
datnd10 <datndhe172134@fpt.edu.vn>
2025-06-30 16:26:27 +0700

    feat: paginate account pages and assign role to account

--- src/main/java/vn/edu/fpt/medicaldiagnosis/service/AccountService.java
+++ src/main/java/vn/edu/fpt/medicaldiagnosis/service/AccountService.java
@@ -1,11 +1,13 @@
 package vn.edu.fpt.medicaldiagnosis.service;
 
 
+import org.springframework.data.domain.Page;
 import vn.edu.fpt.medicaldiagnosis.dto.request.AccountCreationRequest;
 import vn.edu.fpt.medicaldiagnosis.dto.request.AccountUpdateRequest;
 import vn.edu.fpt.medicaldiagnosis.dto.response.AccountResponse;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AccountService {
 
@@ -22,4 +24,8 @@ public interface AccountService {
     AccountResponse getMyInfo();
 
     String generateUniqueUsername(String firstName, String middleName, String lastName);
+
+    Page<AccountResponse> getAccountsPaged(Map<String, String> filters, int page, int size, String sortBy, String sortDir);
+
+    AccountResponse assignRoles(String accountId, List<String> roleNames);
 }
