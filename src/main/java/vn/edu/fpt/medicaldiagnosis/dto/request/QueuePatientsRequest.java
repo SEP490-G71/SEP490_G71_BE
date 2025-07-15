2b3f5ccd064b1a0978d8bd2cb306cf62c0b3db21
manhminh <manhminh2601@gmail.com>
2025-07-15 23:15:52 +0700

    fix: fix assign room automically

--- src/main/java/vn/edu/fpt/medicaldiagnosis/dto/request/QueuePatientsRequest.java
+++ src/main/java/vn/edu/fpt/medicaldiagnosis/dto/request/QueuePatientsRequest.java
@@ -2,28 +2,45 @@ package vn.edu.fpt.medicaldiagnosis.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import vn.edu.fpt.medicaldiagnosis.enums.DepartmentType;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class QueuePatientsRequest {
+
     private String queueId;
+
+    @NotNull(message = "PATIENT_ID_REQUIRED")
     private String patientId;
+
     private Long queueOrder;
+
     private String status;
+
     private LocalDateTime checkinTime;
+
     private LocalDateTime checkoutTime;
+
     private String roomNumber;
+
+    @NotNull(message = "DEPARTMENT_TYPE_EMPTY")
     @JsonProperty
     private DepartmentType type;
+
     private LocalDateTime calledTime;
+
     private Boolean isPriority;
 
+    @NotNull(message = "REGISTERED_TIME_REQUIRED")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime registeredTime;
 }
